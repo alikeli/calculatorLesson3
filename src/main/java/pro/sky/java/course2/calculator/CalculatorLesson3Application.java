f3215297d1a1c6697973a4e8985fae84d660682a
alikeli <eli-com2@yandex.ru>
2022-09-18 20:25:59 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorLesson3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorLesson3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorLesson3Application.class, args);
+    }
+
+}
